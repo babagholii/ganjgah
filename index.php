be84fc329d18da7f168f5439e3c0996c21ba8871
Miladcloudy <Miladcloudy@hotmail.com>
2022-01-12 16:35:02 +0330

    Added index.php

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+  <!-- Favicon and touch icons -->
+  <!--  <link rel="shortcut icon" href="--><? //= baseUrl() ?><!--/image/project/logo-64.png">-->
+
+  <meta charset="utf-8">
+  <title></title>
+
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="asset/style/style.css">
+  <!--  <script src="--><? //= baseUrl() ?><!--/theme/page-home/asset/js/custom.js"></script>-->
+
+</head>
+<body>
+
+<div class="header">
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+
+      <a class="navbar-brand" href="">
+        <img src="image/logo-g.png" style="height: 100px;" alt="">
+      </a>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="" class="nav-link smoothScroll">فال حافظ</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="" class="nav-link">گنجینه اشعار</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="" class="nav-link">درباره ما</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
+
+<div class="content">
+  <div class="image-left-background"></div>
+
+  <div class="content-item">
+    <img src="image/index-book.png" style="width: 300px">
+
+    <div style="display: inline-block; margin-right: 15px">
+      <pre class="block-justify" style="padding: 8px; margin-top: 25px;">
+تاریخ کهن ایران زمین با پیشینه ای غنی از فرهنگ و هنر، از دیرباز با شعر و ادبیات در هم
+پیجیده و ایران، مهد پرورش شاعران بزرگی بوده و درگیر و دار فراز و نشیب هایی که در طول
+تاریخ بر آن گذشته، اما شاهنامه وزین و پارسی پرور فردوسی بزرگ، اشعار روح بخش مولانا و
+عش بازی اش با حق، غزل های عاشقانه و عارفانه عالم گیر در دیوان حافظ، اندیشه ژرف و عرفان
+ناب عطار نیشابوری همه و همه همچون مرهمی بر زخم های کوچک و بزرگش تسکین داده اند.
+      </pre>
+
+      <div style="text-align: center">
+        <button id="btn-login" type="submit" class="btn" name="btn-submit" style="vertical-align: middle;">فال حافظ</button>
+        <button id="btn-login" type="submit" class="btn" name="btn-submit" style="vertical-align: middle;">گنجینه اشعار</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="footer-wrapper">
+  <div id="footer-design">@2021 Tala Alikhasi & Masume Godarzi, All Rights Reserved</div>
+</div>
+
+</body>
+</html>
