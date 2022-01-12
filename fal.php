2a050001660fd18e944c2a867956f9a0b6af0875
Miladcloudy <Miladcloudy@hotmail.com>
2022-01-12 21:31:35 +0330

    Added fal.php

---
+++ fal.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+  <!-- Favicon and touch icons -->
+  <!--  <link rel="shortcut icon" href="--><? //= baseUrl() ?><!--/image/project/logo-64.png">-->
+
+  <meta charset="utf-8">
+  <title></title>
+
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="asset/style/style.css">
+  <!--  <script src="--><? //= baseUrl() ?><!--/theme/page-home/asset/js/custom.js"></script>-->
+
+</head>
+<body>
+<div class="fal-content-body">
+  <div class="fal-content">
+    <div class="fal-part">
+      <div class="fal-ghazal-number-part">
+        <div class="fal-ghazal-number-lbl"><span>غزل شماره </span><span>28</span><span> :</span></div>
+
+        <div class="fal-ghazal-number-text">
+          <pre>
+            نسیم باد صبا دوشم آگهی آورد
+          </pre>
+        </div>
+
+        <div class="fal-ghazal-music-prat"></div>
+      </div>
+
+      <div class="fal-ghazal-text-part">
+        <div class="fal-ghazal-text-lbl">
+          <span>ای صاحب فال: </span>
+        </div>
+
+        <div class="fal-ghazal-text">
+          <pre>
+روزهای غم و سختی به زودی تمام خواهد شد و خبرهای خوشی به شما
+میرسد. چنان ذوق زده خواهی شد که برای خودتان هم غیر قابل باور است
+مطمن باش که دیگر ناراحتی های تو دوام ندارد و به زودی روزگار خوشی فرا
+خواهد رسید.
+          </pre>
+        </div>
+      </div>
+
+      <button class="fal-btn">بازگشت به صفحه اصلی</button>
+    </div>
+  </div>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -16,60 +16,61 @@
 </head>
 <body>
 
-<div class="header">
-  <nav class="navbar navbar-expand-lg">
-    <div class="container">
+<div class="content-body">
+  <div class="header">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
 
-      <a class="navbar-brand" href="">
-        <img src="image/logo-g.png" style="height: 100px;" alt="">
-      </a>
+        <a class="navbar-brand" href="">
+          <img src="image/logo-g.png" style="height: 100px;width: 100px;" alt="">
+        </a>
 
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a href="" class="nav-link smoothScroll">فال حافظ</a>
-          </li>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="" class="nav-link smoothScroll">فال حافظ</a>
+            </li>
 
-          <li class="nav-item">
-            <a href="" class="nav-link">گنجینه اشعار</a>
-          </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">گنجینه اشعار</a>
+            </li>
 
-          <li class="nav-item">
-            <a href="" class="nav-link">درباره ما</a>
-          </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">درباره ما</a>
+            </li>
 
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-</div>
+    </nav>
+  </div>
 
-<div class="content">
-  <div class="image-left-background"></div>
+  <div class="content">
+    <div class="image-left-background"></div>
 
-  <div class="content-item">
-    <img src="image/index-book.png" style="width: 300px">
+    <div class="content-item">
+      <img src="image/index-book.png" class="img-index-book">
 
-    <div style="display: inline-block; margin-right: 10px">
-      <pre class="block-justify" style="padding: 8px; margin-top: 25px;">
-تاریخ کهن ایران زمین با پیشینه ای غنی از فرهنگ و هنر، از دیرباز با شعر و ادبیات در هم
-پیجیده و ایران، مهد پرورش شاعران بزرگی بوده و درگیر و دار فراز و نشیب هایی که در طول
-تاریخ بر آن گذشته، اما شاهنامه وزین و پارسی پرور فردوسی بزرگ، اشعار روح بخش مولانا و
-عش بازی اش با حق، غزل های عاشقانه و عارفانه عالم گیر در دیوان حافظ، اندیشه ژرف و عرفان
-ناب عطار نیشابوری همه و همه همچون مرهمی بر زخم های کوچک و بزرگش تسکین داده اند.
-      </pre>
+      <div style="display: inline-block; margin-right: 10px">
+        <pre class="block-justify" style="padding: 8px; margin-top: 25px;">
+  تاریخ کهن ایران زمین با پیشینه ای غنی از فرهنگ و هنر، از دیرباز با شعر و ادبیات در هم
+  پیجیده و ایران، مهد پرورش شاعران بزرگی بوده و درگیر و دار فراز و نشیب هایی که در طول
+  تاریخ بر آن گذشته، اما شاهنامه وزین و پارسی پرور فردوسی بزرگ، اشعار روح بخش مولانا و
+  عش بازی اش با حق، غزل های عاشقانه و عارفانه عالم گیر در دیوان حافظ، اندیشه ژرف و عرفان
+  ناب عطار نیشابوری همه و همه همچون مرهمی بر زخم های کوچک و بزرگش تسکین داده اند.
+        </pre>
 
-      <div style="text-align: center">
-        <button id="btn-login" type="submit" class="btn" name="btn-submit" style="vertical-align: middle; margin-left: 10px">فال حافظ</button>
-        <button id="btn-login" type="submit" class="btn" name="btn-submit" style="vertical-align: middle;">گنجینه اشعار</button>
+        <div style="text-align: center">
+          <button id="btn-login" type="submit" class="btn" name="btn-submit" style="vertical-align: middle; margin-left: 10px">فال حافظ</button>
+          <button id="btn-login" type="submit" class="btn" name="btn-submit" style="vertical-align: middle;">گنجینه اشعار</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<div id="footer-wrapper">
-  <div id="footer-design">@2021 Tala Alikhasi & Masume Godarzi, All Rights Reserved</div>
+  <div id="footer-wrapper">
+    <div id="footer-design">@2021 Tala Alikhasi & Masume Godarzi, All Rights Reserved</div>
+  </div>
 </div>
-
 </body>
 </html>
